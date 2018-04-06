69b23a18197e1400bcf499387cdeb61c01949f99
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-04-06 11:12:31 -0700

    more styling

--- frontend/components/projects/project_item.jsx
+++ frontend/components/projects/project_item.jsx
@@ -15,12 +15,12 @@ class ProjectItemView extends React.Component {
           <img src={this.props.project.image_url}/>
           <div className="header-explore-project-item">
             <h4>{this.props.project.title}</h4>
-            <h6>by: {this.props.project.username}}</h6>
+            <h6>by: {this.props.project.username}</h6>
           </div>
           <div className="botton-explore-project-item">
-            <h3>$86,260pledged</h3>
-            <h4>862%funded</h4>
-            <h4>20days to go</h4>
+            <h3>"$86,260  pledged"</h3>
+            <h4>"862% funded"</h4>
+            <h4>"20days to go"</h4>
             <h5>Gadgets</h5>
           </div>
         </section>
