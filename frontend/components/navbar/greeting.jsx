57366fe99208ecf38033255049e02969101f3fd0
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-04-09 15:07:53 -0700

    refactor greetings

--- frontend/components/navbar/greeting.jsx
+++ frontend/components/navbar/greeting.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const sessionLinks = () => (
-  <Link to="/login">Login</Link>
-);
 
-const personalGreeting = (currentUser, logout) => (
-    <a className="nav-logout" onClick={logout}>Log out</a>
-);
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-const Greeting = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+  handleLogin() {
+    // 1. dispatch to ui the url: how to get url? params. withrouter./ Route.
+    // 2. redirect to login.
+  }
+
+  logoutUser() {
+    this.props.logout();
+  }
+
+  render() {
+    return (
+      <div>
+      {this.props.currentUser ? (<a className="nav-logout" onClick={() => this.logoutUser() }>Log out</a>) : (<Link to="/login">Login</Link>)}
+      </div>
+    )
+  }
+}
+
 
 export default Greeting;
