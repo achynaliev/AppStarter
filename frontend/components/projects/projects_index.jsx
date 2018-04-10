cd4d580bc49c6d77ed62536fe0bfbc61032bdf53
Atai Chynaliev <chynaliev.ae@gmail.com>
2018-04-09 17:19:02 -0700

    explore styling

--- frontend/components/projects/projects_index.jsx
+++ frontend/components/projects/projects_index.jsx
@@ -22,15 +22,17 @@ class ProjectsIndex extends React.Component {
     if (this.props.projects) {
       return (
         <section className="main-body">
-        <h2>Show me<select
-          value={this.state.category}
-          onChange={this.update('category')}
-          className="category-filter">
-          <option value="all">all</option>
-          <option value="tech">technology</option>
-          <option value="design">design</option>
-          <option value="gadets">gadets</option>
-        </select>projects on earth</h2>
+          <div className="explore-filter">
+            <h2>Show me    <select
+              value={this.state.category}
+              onChange={this.update('category')}
+              className="category-filter">
+              <option value="all">all</option>
+              <option value="tech">technology</option>
+              <option value="design">design</option>
+              <option value="gadets">gadets</option>
+            </select>    projects on earth</h2>
+          </div>
         {this.state.category === 'all' ?
         (<AllProjectCategories projects={this.props.projects} category={this.state.category} />) :
         (<ProjectCategories projects={this.props.projects} category={this.state.category} />)}
